550912a474ccc1ba67e5aeec63c70220c407cfbf
Xaninho <neves.27.17@gmail.com>
2021-02-01 18:07:05 +0000

    Register & Login Users

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,5 +4,8 @@ const userResolvers = require('./users.js');
 module.exports = {
     Query: {
         ...postResolvers.Query
+    },
+    Mutation: {
+        ...userResolvers.Mutation
     }
-}
+};
