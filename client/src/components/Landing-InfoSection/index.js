e2148d0b158d3a9d6bcb61a5fa1b33b240aa57c7
Xaninho <neves.27.17@gmail.com>
2021-02-18 17:49:44 +0000

    Info Section

---
+++ client/src/components/Landing-InfoSection/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Button } from 'react-scroll'
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElements'
+
+const InfoSection = () => {
+    return (
+        <div>
+            <InfoContainer>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column1>
+                        <TextWrapper>
+                            <TopLine>
+                                Topline
+                            </TopLine>
+                            <Heading>
+                                Heading
+                            </Heading>
+                            <Subtitle>
+
+                            </Subtitle>
+                            <BtnWrap>
+                                <Button to="home" />
+                            </BtnWrap>
+                        </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>
+        </div>
+    )
+}
+
+export default InfoSection
