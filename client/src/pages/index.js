f8cff65063aced7cd52f7d03f3889a7f58fbc3f7
Xaninho <neves.27.17@gmail.com>
2021-02-18 15:52:22 +0000

    Hero Section

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Sidebar from '../components/Landing-Sidebar'
 import Navbar from '../components/Landing-Navbar';
+import HeroSection from '../components/Landing-HeroSection';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -13,6 +14,7 @@ const Home = () => {
         <div>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle}/>
+            <HeroSection />
         </div>
     )
 }
