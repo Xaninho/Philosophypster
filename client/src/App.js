165bb9cec2692e422b5fb27dfdfa39935ab4ae7e
Xaninho <neves.27.17@gmail.com>
2021-03-31 20:56:31 +0100

    Material UI Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Container } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-import { AuthProvider } from './context/auth';
-import AuthRoute from './util/AuthRoute';
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
 
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import MenuBar from './components/dashboard/MenuBar';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import SinglePost from './pages/SinglePost';
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import MenuBar from "./components/dashboard/MenuBar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import SinglePost from "./pages/SinglePost";
+
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00a86b",
+    },
+    secondary: {
+      main: "#4b59f7",
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact/>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
 
-        <AuthProvider>
-          <Container>
-            <MenuBar />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <AuthRoute exact path="/login" component={Login} />
-            <AuthRoute exact path="/register" component={Register} />
-            <Route exact path="/posts/:postId" component={SinglePost} />
-          </Container>
-        </AuthProvider>
-        
-      </Switch>
-    </Router>
+          <AuthProvider>
+            <Container>
+              <MenuBar />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <AuthRoute exact path="/login" component={Login} />
+              <AuthRoute exact path="/register" component={Register} />
+              <Route exact path="/posts/:postId" component={SinglePost} />
+            </Container>
+          </AuthProvider>
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
